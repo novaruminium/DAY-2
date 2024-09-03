4264ff351448a4a81c1a3ea89ea22f28a4c4215f
juanangelo <juanangelo.second@gmail.com>
2024-09-03 12:18:41 +0700

    new upgrade for stability

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("My name is Juan");
+        System.out.println("This is the new upgrade");
     }
 }
