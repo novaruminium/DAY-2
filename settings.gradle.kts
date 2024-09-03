2da4fab8413805b4c071bfc2828093d2bbdf2beb
juanangelo <juanangelo.second@gmail.com>
2024-09-03 14:03:46 +0700

    Task 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
 rootProject.name = "MODULE-1"
-
+include("src:main:TEST-2")
+findProject(":src:main:TEST-2")?.name = "TEST-2"
+include("src:TEST-2")
+findProject(":src:TEST-2")?.name = "TEST-2"
---
+++ src/TEST-2/src/main/java/org/example/task1.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class task1 {
+    public static void main(String[] args) {
+        double farenheit = 1;
+        double celcius = -17.22;
+    }
+}
